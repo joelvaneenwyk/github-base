2b15fa6dfbb5153df76d1540d38132bfc507c0cd
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2025-02-14 23:09:34 -0800

    chore: integrate TypeScript support and update ESLint configuration
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,9 @@
 import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-});
-
-export default [
-  ...compat.extends('eslint:recommended'),
+export default tseslint.config(
+  js.configs.recommended,
   {
     languageOptions: {
       globals: {
@@ -235,5 +225,9 @@ export default [
         }
       }
     }
+  },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}']
   }
-];
+);
